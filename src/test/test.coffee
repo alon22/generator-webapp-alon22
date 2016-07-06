5f58ef03aa15c13b8a0d48cb3e8145f76e7428a9
alon22 <ralonso022@gmail.com>
2016-07-06 18:08:52 +0200

    implement tests

--- src/test/test.coffee
+++ src/test/test.coffee
@@ -1,3 +1,34 @@
 path    = require 'path'
 assert  = require 'assert'
 helpers = require('yeoman-generator').test
+
+describe "generator webapp-alon22", ->
+  before ->
+    @app = helpers
+      .run require.resolve '../app'
+      .withGenerators [
+         [helpers.createDummyGenerator(), 'webapp-alon22:app']
+      ]
+      .withOptions
+        'skip-install' : true
+      .withPrompts
+        projectName : 'testGenerator'
+      return
+    return
+
+  describe 'default settings', ->
+    before (done) ->
+      @app.on 'end', done
+
+    it 'creates expected files', ->
+      expected = [
+        'src/jade/index.jade'
+        'src/stylus/app.styl'
+        'src/coffee/app.coffee'
+        'bower.json'
+        '.gitignore'
+        'package.json'
+        'gruntfile.coffee'
+      ]
+      assert.file expected
+      return
--- test/test.js
+++ test/test.js
@@ -1,4 +1,31 @@
 (function() {
+  var assert, helpers, path;
 
+  path = require('path');
+
+  assert = require('assert');
+
+  helpers = require('yeoman-generator').test;
+
+  describe("generator webapp-alon22", function() {
+    before(function() {
+      this.app = helpers.run(require.resolve('../app')).withGenerators([[helpers.createDummyGenerator(), 'webapp-alon22:app']]).withOptions({
+        'skip-install': true
+      }).withPrompts({
+        projectName: 'testGenerator'
+      });
+      return;
+    });
+    return describe('default settings', function() {
+      before(function(done) {
+        return this.app.on('end', done);
+      });
+      return it('creates expected files', function() {
+        var expected;
+        expected = ['src/jade/index.jade', 'src/stylus/app.styl', 'src/coffee/app.coffee', 'bower.json', '.gitignore', 'package.json', 'gruntfile.coffee'];
+        assert.file(expected);
+      });
+    });
+  });
 
 }).call(this);
